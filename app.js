e99593ec3c11ea17dded4efb531cd9f8a761281d
Tracy <traciinguyen@hotmail.com>
2025-10-21 00:17:18 -0400

    Init app

---
+++ app.js
@@ -0,0 +1,40 @@
+import createHttpError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+const app = express();
+const PORT = 8080;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(import.meta.dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createHttpError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+export default app;
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).send("Home");
+});
+
+export default router;
